a3eaba9bb60f00fbfe8a27f56b116b84daa4d91f
Dan <dmennella34@gmail.com>
2019-09-24 16:23:40 -0400

    added static

---
+++ build/precache-manifest.a49e9ddee785712414a31ee27e667df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681d509ce4b5277e39109ef79ad9de36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4584e0562d4b64d5603a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "195c65e4a0654e88d79c",
+    "url": "/static/js/2.07cba69e.chunk.js"
+  },
+  {
+    "revision": "4584e0562d4b64d5603a",
+    "url": "/static/js/main.7df36b68.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
